b2025364bd76803a61ef179e5ee1b01ac08db110
Jake <kubazahora@gmail.com>
2021-10-23 22:00:38 -0500

    replaced array objects with json

--- mapmania2/map-mania2.js
+++ mapmania2/map-mania2.js
@@ -33,7 +33,7 @@ function initMap() {
   }
   $.getJSON('places.json', function(data) {
     places = data;
-    console.log(places[place1]);
+    console.log(places.filter(obj => {return obj.content === "Krakow, Poland"}));
 });
 }
 
